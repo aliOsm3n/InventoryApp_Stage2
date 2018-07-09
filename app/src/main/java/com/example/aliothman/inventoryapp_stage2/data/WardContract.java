27b73e48d179e3a677c076774dbd9516b30e09e8
aliOsm3n <ali36036samor@gmail.com>
2018-07-09 10:05:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/aliothman/inventoryapp_stage2/data/WardContract.java
@@ -0,0 +1,30 @@
+package com.example.aliothman.inventoryapp_stage2.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class WardContract {
+    public static final String CONTENT_AUTHORITY = "com.example.aliothman.inventoryapp_stage2";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PRODUCTS = ProductEntry.TABLE_NAME;
+
+    public static abstract class ProductEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+        public static final String TABLE_NAME = "products";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_IMAGE = "image";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_AVAILABLE_QUANTITY = "availableQuantity";
+        public static final String COLUMN_PRODUCT_SUPPLIER_NAME = "supplierName";
+        public static final String COLUMN_PRODUCT_SUPPLIER_EMAIL = "supplierEmail";
+        public static final String COLUMN_PRODUCT_SUPPLIER_PHONE_NUMBER = "supplierPhoneNumber";
+}
+}
